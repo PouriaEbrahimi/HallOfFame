cf286f8971aa1c16fd942aa4701c7b5a35873429
Pouria Ebrahimi <ebra0008@gmail.com>
2017-07-16 08:08:19 +0000

    Add validations to Memers controller

--- app/models/memer.rb
+++ app/models/memer.rb
@@ -1,2 +1,10 @@
 class Memer < ActiveRecord::Base
+    validates :name, :presence => true, 
+    format: { with: /\A[a-zA-Z]*+\z/, message: "only uses alphabet characters without spaces" }
+    
+    validates :age, :presence => true
+    validates_numericality_of :age, only_integer: true, message: "only uses numbers"
+    
+    validates :gender, :presence => true, 
+    format: { with: /\A[a-zA-Z0-9 ]*+\z/, message: "only uses alphanumeric characters and spaces" }
 end
